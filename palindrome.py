f68695a885c94f26e4aafbad698322144cdfde74
joczarnocka <42293202+joczarnocka@users.noreply.github.com>
2022-07-26 19:30:54 +0200

    add documentation

--- palindrome.py
+++ palindrome.py
@@ -1,2 +1,9 @@
 def is_palindrom(word):
+    """
+        Function detects whether a given word is palindrome,
+        which means that reading it from beginning
+        and from end returns the same value
+        Arguments:
+        word
+    """
     return word[::-1] == word
